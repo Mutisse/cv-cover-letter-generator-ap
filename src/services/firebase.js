eac44533a254b0e3a71c12230c217756d6304ed8
Edilson Mutisse <98753172+Mutisse@users.noreply.github.com>
2026-06-08 23:12:10 +0200

    Configura Firebase com credenciais reais

--- src/services/firebase.js
+++ src/services/firebase.js
@@ -1,11 +1,9 @@
-// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
+import { getStorage } from "firebase/storage";
 
 // Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
   apiKey: "AIzaSyBhnPPF9Lnxuvk9uuS97g66QnxSnM3Ycag",
   authDomain: "cv-osorio.firebaseapp.com",
@@ -18,4 +16,10 @@ const firebaseConfig = {
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
+
+// Initialize services
+export const auth = getAuth(app);
+export const db = getFirestore(app);
+export const storage = getStorage(app);
+
+export default app;
